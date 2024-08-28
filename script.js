02dfaf3765dea244b3c2689fd24b7173e7427bdf
Mohan Dev <bhardwaj1206409@gmail.com>
2024-08-28 22:15:14 +0530

    initialized events  after DOMcontent loaded event

--- script.js
+++ script.js
@@ -1,167 +1,167 @@
-const inputContainer = document.querySelector(".text-input-container");
-const INPUT_CLASS = "input-box";
-const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const KEY_CODES = {
-    BACKSPACE_KEY: "Backspace",
-    ARROW_LEFT: "ArrowLeft",
-    ARROW_RIGHT: "ArrowRight",
-    DIGITS: "Digit",
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const inputContainer = document.querySelector(".text-input-container");
+    const INPUT_CLASS = "input-box";
+    const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const KEY_CODES = {
+        BACKSPACE_KEY: "Backspace",
+        ARROW_LEFT: "ArrowLeft",
+        ARROW_RIGHT: "ArrowRight",
+        DIGITS: "Digit",
+    };
 
-const LEFT = "left";
-const RIGHT = "right";
-const clearButton = document.querySelector('.clear-btn');
-const firstBox = document.querySelector(".input-box");
+    const LEFT = "left";
+    const RIGHT = "right";
+    const clearButton = document.querySelector('.clear-btn');
+    const firstBox = document.querySelector(".input-box");
 
 
-firstBox.focus();
+    firstBox.focus();
 
-const handleKeydown = (e, elementId) => {
-    if (e.code === KEY_CODES.BACKSPACE_KEY) {
-        e.preventDefault();
-        if (e.target.value) {
-            e.target.value = "";
-            return;
-        }
+    const handleKeydown = (e, elementId) => {
+        if (e.code === KEY_CODES.BACKSPACE_KEY) {
+            e.preventDefault();
+            if (e.target.value) {
+                e.target.value = "";
+                return;
+            }
 
-        //
-        if (!e.target.value) {
             moveToPreviousInputBox(elementId, e);
+
         }
-    }
 
-    if (e.code === KEY_CODES.ARROW_LEFT) {
-        moveToPreviousInputBox(elementId);
-    }
-    if (e.code === KEY_CODES.ARROW_RIGHT) {
-        moveToNextInputBox(elementId);
-    }
-    if (e.code.includes(KEY_CODES.DIGITS)) {
-        e.preventDefault();
-        const value = e.code.split("Digit")?.[1];
-        e.target.value = value;
-        moveToNextInputBox(elementId);
-    }
-};
+        if (e.code === KEY_CODES.ARROW_LEFT) {
+            moveToPreviousInputBox(elementId);
+        }
+        if (e.code === KEY_CODES.ARROW_RIGHT) {
+            moveToNextInputBox(elementId);
+        }
+        if (e.code.includes(KEY_CODES.DIGITS)) {
+            e.preventDefault();
+            const value = e.code.split("Digit")?.[1];
+            e.target.value = value;
+            moveToNextInputBox(elementId);
+        }
+    };
 
-const extractSiblingId = (currentId, siblingSide) => {
-    if (siblingSide === RIGHT) {
-        return currentId.split("-")[0] + "-" + Number(+currentId.split("-")[1] + 1);
-    } else if (siblingSide === LEFT) {
-        return currentId.split("-")[0] + "-" + Number(+currentId.split("-")[1] - 1);
-    }
-};
+    const extractSiblingId = (currentId, siblingSide) => {
+        if (siblingSide === RIGHT) {
+            return currentId.split("-")[0] + "-" + Number(+currentId.split("-")[1] + 1);
+        } else if (siblingSide === LEFT) {
+            return currentId.split("-")[0] + "-" + Number(+currentId.split("-")[1] - 1);
+        }
+    };
 
-const moveToNextInputBox = (currentId) => {
-    if (Number(currentId.split("-")?.[1]) === 5) return;
+    const moveToNextInputBox = (currentId) => {
+        if (Number(currentId.split("-")?.[1]) === 5) return;
 
-    const nextElementId = extractSiblingId(currentId, RIGHT);
-    const nextElement = document.getElementById(nextElementId);
-    nextElement.focus();
-};
+        const nextElementId = extractSiblingId(currentId, RIGHT);
+        const nextElement = document.getElementById(nextElementId);
+        nextElement.focus();
+    };
 
-const moveToPreviousInputBox = (currentId) => {
-    if (Number(currentId.split("-")?.[1]) === 1) return;
+    const moveToPreviousInputBox = (currentId) => {
+        if (Number(currentId.split("-")?.[1]) === 1) return;
 
-    const prevElementId = extractSiblingId(currentId, LEFT);
-    const prevElement = document.getElementById(prevElementId);
-    prevElement.focus();
-};
+        const prevElementId = extractSiblingId(currentId, LEFT);
+        const prevElement = document.getElementById(prevElementId);
+        prevElement.focus();
+    };
 
-const getElementAndId = (e) => {
-    if (e.target.className === INPUT_CLASS) {
-        const element = e.target;
-        const elementId = e.target.id;
+    const getElementAndId = (e) => {
+        if (e.target.className === INPUT_CLASS) {
+            const element = e.target;
+            const elementId = e.target.id;
 
-        return {
-            element,
-            elementId,
-        };
-    }
-};
-const handleInputChange = (e, elementId) => {
-    if (e?.data) {
-        if (e?.data?.length > 1) return;
-        const value = e.target.value;
-        const regex = /^[0-9]?$/;
+            return {
+                element,
+                elementId,
+            };
+        }
+    };
+    const handleInputChange = (e, elementId) => {
+        if (e?.data) {
+            if (e?.data?.length > 1) { return };
+            const value = e.target.value;
+            const regex = /^[0-9]?$/;
 
-        if (!regex.test(value)) {
-            e.target.value = value.slice(0, -1);
-            // Remove the last entered character if it's not numeric
+            if (!regex.test(value)) {
+                e.target.value = value.slice(0, -1);
+                // Remove the last entered character if it's not numeric
+                if (nums.includes(value)) moveToNextInputBox(elementId);
+            }
             if (nums.includes(value)) moveToNextInputBox(elementId);
         }
-        if (nums.includes(value)) moveToNextInputBox(elementId);
-    }
-};
+    };
 
-const handlePasting = (event, pasteText, elementId) => {
-    const startIndex = Number(elementId?.split("-")[1]);
+    const handlePasting = (event, pasteText, elementId) => {
+        const startIndex = Number(elementId?.split("-")[1]);
 
-    const requiredELements = [];
-    for (i = startIndex; i <= 5; i++) {
-        const ele = document.getElementById(`input-${i}`);
-        requiredELements.push(ele);
-    }
-    requiredELements.forEach((ele, index) => {
-        if (pasteText[index]) {
-            ele.value = pasteText[index];
-            moveToNextInputBox(ele?.id);
+        const requiredELements = [];
+        for (i = startIndex; i <= 5; i++) {
+            const ele = document.getElementById(`input-${i}`);
+            requiredELements.push(ele);
         }
-    });
-};
+        requiredELements.forEach((ele, index) => {
+            if (pasteText[index]) {
+                ele.value = pasteText[index];
+                moveToNextInputBox(ele?.id);
+            }
+        });
+    };
 
-const showInputError = (e) => {
-    e.stopPropagation();
-    const errorElement = document.querySelector(".error");
-    errorElement.textContent =
-        "Please Enter a valid Input, Only Numeric Values are allowed!";
-};
-inputContainer.addEventListener(
-    "input",
-    (e) => {
-        const { element, elementId } = getElementAndId(e);
-        handleInputChange(e, elementId);
-    },
-    true
-);
+    const showInputError = (e) => {
+        e.stopPropagation();
+        const errorElement = document.querySelector(".error");
+        errorElement.textContent =
+            "Please Enter a valid Input, Only Numeric Values are allowed!";
+    };
+    inputContainer.addEventListener(
+        "input",
+        (e) => {
+            const { element, elementId } = getElementAndId(e);
+            handleInputChange(e, elementId);
+        },
+        true
+    );
 
-inputContainer.addEventListener(
-    "paste",
-    (e) => {
-        const { element, elementId } = getElementAndId(e);
+    inputContainer.addEventListener(
+        "paste",
+        (e) => {
+            const { element, elementId } = getElementAndId(e);
 
-        let pasteText = (e.clipboardData || window?.clipboardData).getData("text");
-        e.preventDefault();
+            let pasteText = (e.clipboardData || window?.clipboardData).getData("text");
+            e.preventDefault();
 
-        // if pasted content has alphabets, we dont allow pasting and show a warning
-        // message that alphabets not allowed.
-        const errorInString =
+            // if pasted content has alphabets, we dont allow pasting and show a warning
+            // message that alphabets not allowed.
+            const errorInString =
+                pasteText
+                    .split("")
+                    .map((ele) => nums.includes(ele))
+                    .filter((ele) => !ele).length > 0;
+
             pasteText
                 .split("")
-                .map((ele) => nums.includes(ele))
-                .filter((ele) => !ele).length > 0;
+                .filter((ele) => ele)
+                .forEach((ele) => {
+                    if (!nums.includes(ele)) {
+                        showInputError(e);
+                    }
+                });
 
-        pasteText
-            .split("")
-            .filter((ele) => ele)
-            .forEach((ele) => {
-                if (!nums.includes(ele)) {
-                    showInputError(e);
-                }
-            });
+            if (!errorInString) handlePasting(e, pasteText, elementId);
+        },
+        true
+    );
 
-        if (!errorInString) handlePasting(e, pasteText, elementId);
-    },
-    true
-);
+    inputContainer.addEventListener('keydown', (e) => {
+        const { elementId } = getElementAndId(e);
+        handleKeydown(e, elementId);
+    }, true)
 
-inputContainer.addEventListener('keydown', (e) => {
-    const { elementId } = getElementAndId(e);
-    handleKeydown(e, elementId);
-}, true)
+    clearButton.addEventListener('click', () => {
+        document.querySelectorAll('.input-box').forEach(ele => ele.value = "");
+        firstBox.focus();
+    })
 
-clearButton.addEventListener('click', () => {
-    document.querySelectorAll('.input-box').forEach(ele => ele.value = "");
-    firstBox.focus();
-})
-
+});
